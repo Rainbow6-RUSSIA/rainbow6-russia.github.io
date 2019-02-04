7348d382a2b754beebcd03c4db8b05c70b74f339
BadCoder1337 <anton.tiz.musin@gmail.com>
2019-02-04 22:30:20 +0300

    map vote done

--- streams/js/map_vote.js
+++ streams/js/map_vote.js
@@ -1,4 +1,34 @@
-socket = io.connect('http://localhost:3001');
+socket = io.connect('http://192.168.0.130:3001');
+
+function syncData(data) {
+    let length = data.pool.length + data.votes.length;
+    console.log('TCL: map_vote', data);
+    if (data.poolCache.length) {
+        data.pool = data.poolCache;
+        length = data.poolCache.length;
+    }
+    data.votes.forEach((v, i, a) => {
+        console.log('vote', i);
+        let el;
+        if (i !== length - 1) {
+            el = $(`#${i}.log-maps > .log-action`);
+            el.css('background-image', `url(${data.poolCache.find(p => p.id === v.mapId).splash})`);
+            $(`#${i}.log-maps > .log-action > .glyph-placeholder > i`).text(v.type === 'ban' ? 'clear' : 'check');
+            el.addClass(v.type === 'ban' ? 'log-action-red' : 'log-action-green');
+            $(`#${i}.log-maps > .log-action > .team-action-text`).text(data.poolCache.find(p => p.id === v.mapId).titleRu.toUpperCase());
+            $(`#${i}.log-maps`).fadeTo(150, 1);
+            let e = $(`.footer-overlay`).eq(data.poolCache.findIndex(m => m.id === v.mapId));
+            e.css('background-image', `url(${data[`team${i % 2}`].logo})`);
+            e.fadeTo(150, 1);
+        } else {
+            el = $(`.decider`);
+            el.css('background-image', `url(${data.poolCache.find(p => p.id === v.mapId).splash})`);
+            $(`.decider > .team-action-text`).text(data.poolCache.find(p => p.id === v.mapId).titleRu.toUpperCase());
+            $(`#-1.log`).fadeTo(150, 1);
+            $('.footer-overlay').fadeTo(150, 1);
+        }
+    });
+}
 
 socket.emit('subscribe', {
     id: location.hash.slice(1),
@@ -6,41 +36,50 @@ socket.emit('subscribe', {
 })
 
 socket.on('init', data => {
-    console.log('TCL: data', data);
-    data.teams.forEach((t, i) => {
-        $(`#${i}.team-logo`).attr('src', t.logo || './img/XXX.png');
-        $(`#${i}.team-title-text`).text(t.name);
-    });
+    const length = data.pool.length + data.votes.length;
+    if (data.poolCache.length) {
+        data.pool = data.poolCache;
+    }
+    console.log('TCL: init', data);
+    for (let i = 0; i < 2; i++) {
+        $(`#${i}.team-logo`).attr('src', data[`team${i}`].logo || './img/XXX.png');
+        $(`#${i}.team-title-text`).text(data[`team${i}`].name);
+    }
+
     $(".team-title-text").textfill();
 
     $('.footer-map > img').attr('src', data.pool[0].splash);
     $('.footer-text').text(data.pool[0].titleRu.toUpperCase());
 
-    if ($('.footer-item').length < data.pool.length) {
-        for (let i = 0; i <= (Math.floor(data.pool.length / 2) + 1); i++) {
+    if ($('.footer-item').length < length) {
+        for (let i = 0; i <= length - 4; i++) {
             console.log('cloning', i,'to', i+2);
             $(`#${i}.log-maps`).clone().attr('id', i+2).appendTo(`#${i % 2}.log`);
         }
-        $(`#-1.log > .log-maps`).attr('id', data.pool.length-1)
-        for (let i = 1; i < data.pool.length; i++) {
+        $(`#-1.log > .log-maps`).attr('id', length-1)
+        for (let i = 1; i < length; i++) {
             $('.footer-item').eq(0).clone().appendTo('footer');
             $('.footer-map > img').eq(i).attr('src', data.pool[i].splash);
             $('.footer-text').eq(i).text(data.pool[i].titleRu.toUpperCase());
         }
     }
 
-    if (data.pool.length <= 9) {
+    syncData(data);
+
+    if (length <= 9) {
         $('footer').fadeTo(0, 1);
     }
 });
 
-socket.on('map_vote', data => {
-    data.votes.forEach((v, i, a) => {
-        const el = $(`#${i}.log-maps > .log-action`);
-        el.css('background-image', `url(${v.map.splash})`);
-        $(`#${i}.log-maps > .log-action > .glyph-placeholder > i`).text(v.type === 'ban' ? 'clear' : 'check');
-        el.addClass(v.type === 'ban' ? 'log-action-red' : 'log-action-green');
-    });
-});
+socket.on('swap', (data) => {
+    console.log('TCL: swap', data);
+    for (let i = 0; i < 2; i++) {
+        $(`#${i}.team-logo`).attr('src', data[`team${i}`].logo || './img/XXX.png');
+        $(`#${i}.team-title-text`).text(data[`team${i}`].name);
+    }
+    $(".team-title-text").textfill();
+})
+
+socket.on('map_vote', syncData);
 
 setTimeout(() => $('body').fadeTo(150, 1), 250);

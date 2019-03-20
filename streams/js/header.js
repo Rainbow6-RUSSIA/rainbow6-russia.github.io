25d50fff2f990ac171a7bee203458d7d4d5f533e
BadCoder1337 <anton.tiz.musin@gmail.com>
2019-03-20 17:48:30 +0300

    update

--- streams/js/header.js
+++ streams/js/header.js
@@ -1,5 +1,19 @@
 socket = io.connect('https://streams.rainbow6russia.ru');
 
+socket.emit('subscribe', {
+    id: location.hash.slice(1),
+    room: 'header',
+})
+
+socket.on('swap', (data) => {
+    console.log('TCL: swap', data);
+    for (let i = 0; i < 2; i++) {
+        $(`#${i}.team-logo`).attr('src', data[`team${i}`].logo || './img/XXX.png');
+        $(`#${i}.team-title-text`).text(data[`team${i}`].name);
+    }
+    $(".team-title-text").textfill();
+})
+
 socket.on('header', syncData);
 
 setTimeout(() => $('body').fadeTo(150, 1), 250);
